781bf3aee8d5e067cc2d171a4499ece5fe09b9a3
Corbin <cds@corbinsimpson.com>
2016-11-05 14:22:15 -0700

    Load YAML with OrderedDicts.

--- run.py
+++ run.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 
+import collections
 import yaml
+
+# http://stackoverflow.com/a/21048064/264985
+_mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
+
+def dict_representer(dumper, data):
+    return dumper.represent_dict(data.iteritems())
+
+def dict_constructor(loader, node):
+    return collections.OrderedDict(loader.construct_pairs(node))
+
+yaml.add_representer(collections.OrderedDict, dict_representer)
+yaml.add_constructor(_mapping_tag, dict_constructor)
+
 from flask import Flask
 from leonardo import app
-
 
 app.debug = True
 app.run(host='0.0.0.0')
